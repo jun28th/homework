e83386d5465a9dec4923044c53e9dfb069061c0c
jun28th <doanlevy2806@gmail.com>
2026-07-24 18:15:56 +0700

    setup cart, tax rate, and member discount variables

---
+++ homeworkSession1/W1S1.js
@@ -0,0 +1,12 @@
+const TAX = 0.08;
+let cart = [450.000, 520.000, 550.000];
+
+let isMember = false; 
+ 
+let discount = isMember == true ? 0.05 : 0;
+
+let finalPrice;  
+
+console.log("You pay: " + finalPrice);
+
+
